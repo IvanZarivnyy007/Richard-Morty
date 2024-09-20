c2bc7e6cefdb0c1f3273e5bac95c9214fe0dce79
Ivan Zarivnyy <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-09-20 13:57:08 +0200

    hello

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ const Home = () => {
     <div className="home-page">
       <Stars />
       <h1 className="home-title">The Rick and Morty Experience</h1>
+      <p className="home-help">
+        Email: 'ecsemple95@gmail.com' Password : 'ecsemple95@gmail.com'
+      </p>
     </div>
   );
 };
